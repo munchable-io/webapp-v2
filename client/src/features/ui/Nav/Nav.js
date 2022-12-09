7027d10b99b090c134cf559ebc8db05d03b5d07d
hamzakamran <hamzakwebdev@gmail.com>
2022-12-09 11:21:23 -0600

    Nav links updated

--- client/src/features/ui/Nav/Nav.js
+++ client/src/features/ui/Nav/Nav.js
@@ -4,8 +4,12 @@ import { NavItems, StyledNav } from "./Nav.styled";
 import NavItem from "./NavItem";
 import { useNavigate } from "react-router-dom";
 import useLogout from "../../auth/useLogout";
+import { useSelector } from "react-redux";
+import { getUser } from "../../auth/auth.slice";
 
 const Nav = forwardRef(({ modifyModal }, ref) => {
+	const auth = useSelector(getUser);
+
 	const navigate = useNavigate();
 	const logout = useLogout();
 
@@ -18,6 +22,7 @@ const Nav = forwardRef(({ modifyModal }, ref) => {
 		}
 	};
 
+	// TODO: show nav items based on login status
 	return (
 		<StyledNav ref={ref}>
 			<div className="container">
@@ -27,18 +32,24 @@ const Nav = forwardRef(({ modifyModal }, ref) => {
 						<FiFileText />
 						<p className="sm">Menu</p>
 					</NavItem>
-					<NavItem to="/editor" modifyModal={modifyModal}>
-						<FiEdit />
-						<p className="sm">Menu Editor</p>
-					</NavItem>
-					<NavItem to="/login" modifyModal={modifyModal}>
-						<FiLogIn />
-						<p className="sm">Register / Login</p>
-					</NavItem>
-					<NavItem onClick={handleLogout} modifyModal={modifyModal}>
-						<FiLogOut />
-						<p className="sm">Logout</p>
-					</NavItem>
+					{["admin", "manager"].includes(auth?.role) && (
+						<NavItem to="/editor" modifyModal={modifyModal}>
+							<FiEdit />
+							<p className="sm">Menu Editor</p>
+						</NavItem>
+					)}
+					{!auth?.accessToken && (
+						<NavItem to="/login" modifyModal={modifyModal}>
+							<FiLogIn />
+							<p className="sm">Register / Login</p>
+						</NavItem>
+					)}
+					{auth?.accessToken && (
+						<NavItem onClick={handleLogout} modifyModal={modifyModal}>
+							<FiLogOut />
+							<p className="sm">Logout</p>
+						</NavItem>
+					)}
 				</NavItems>
 			</div>
 		</StyledNav>
