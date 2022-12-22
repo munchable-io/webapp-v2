b7ba91b36cad77452a00dde1d5ffb2acda262751
hamzakamran <hamzakwebdev@gmail.com>
2022-12-21 22:53:18 -0600

    Minor api change

--- server/api/payment/pay/pay.controller.js
+++ server/api/payment/pay/pay.controller.js
@@ -3,42 +3,49 @@ const stripe = require("stripe")(process.env.STRIPE_TEST_SK);
 
 // create payment link
 /*
-    line_items: [{
+    order: {
+		name: String,
+		price: Number,
+		qty: Number,
+		taxRates: [String]
+	}
+*/
+const createPaymentLink = async (req, res) => {
+    const { cancelUrl, successUrl, order } = req.body;
+    if (!cancelUrl || !successUrl || !order) {
+        return res.status(500).json({
+            message: "Please include cancelUrl, successUrl, and order.",
+        });
+    }
+
+    const lineItems = order.map((item) => ({
         price_data: {
             currency: "usd",
             product_data: {
-                name: "product name"
+                name: item?.name,
             },
-            unit_amount: 123, <-- price in cents
+            unit_amount: item?.price,
         },
-        quantity: 1,
-        tax_rates: [tax_rates_ids]
-    }]
-*/
-const createPaymentLink = async (req, res) => {
-	const { cancelUrl, successUrl, lineItems } = req.body;
-	if (!cancelUrl || !successUrl || !lineItems) {
-		return res.status(500).json({
-			message: "Please include cancelUrl, successUrl, and lineItems.",
-		});
-	}
+        quantity: item?.qty,
+        tax_rates: [...item?.taxRates],
+    }));
 
-	try {
-		const session = await stripe.checkout.sessions.create({
-			cancel_url: cancelUrl,
-			success_url: successUrl,
-			mode: "payment",
-			payment_method_types: ["card"],
-			tax_id_collection: {
-				enabled: true,
-			},
-			line_items: lineItems,
-		});
+    try {
+        const session = await stripe.checkout.sessions.create({
+            cancel_url: cancelUrl,
+            success_url: successUrl,
+            mode: "payment",
+            payment_method_types: ["card"],
+            tax_id_collection: {
+                enabled: true,
+            },
+            line_items: lineItems,
+        });
 
-		res.status(200).json({ url: session.url });
-	} catch (err) {
-		res.status(500).json({ message: err.message });
-	}
+        res.status(200).json({ url: session.url });
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 };
 
 module.exports = { createPaymentLink };
